46550ec44e577fdd09fe24f582b8ec501958f7ed
HarshadBhosale <harshad.bhosale@cogoport.com>
2023-02-07 15:29:16 +0530

    Updates

---
+++ precache-manifest.941d28067f5fc95a451b4fc0236a5317.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d738f611ea62a15eca46",
+    "url": "/to_do_react/static/js/main.d738f611.chunk.js"
+  },
+  {
+    "revision": "004a3eadc75e2810afb9",
+    "url": "/to_do_react/static/js/1.004a3ead.chunk.js"
+  },
+  {
+    "revision": "c4ed1d4a8d3006c557b2",
+    "url": "/to_do_react/static/js/runtime~main.c4ed1d4a.js"
+  },
+  {
+    "revision": "a517fc32629e3d2081202b9782b535cb",
+    "url": "/to_do_react/index.html"
+  }
+];
